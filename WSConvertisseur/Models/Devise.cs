543c512aa71ebdab9b461ce46b4a99dff257e707
elhoubn <elhoubn@D350-S03>
2024-01-26 08:44:26 +0100

    ok c'est bon

---
+++ WSConvertisseur/Models/Devise.cs
@@ -0,0 +1,59 @@
+﻿namespace WSConvertisseur.Models
+{
+    public class Devise
+    {
+        int id;
+        string nomDevise;
+        double taux;
+
+        public Devise()
+        {
+        }
+
+        public Devise(int id, string nomDevise, double taux)
+        {
+            this.Id = id;
+            this.NomDevise = nomDevise;
+            this.Taux = taux;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string NomDevise
+        {
+            get
+            {
+                return nomDevise;
+            }
+
+            set
+            {
+                nomDevise = value;
+            }
+        }
+
+        public double Taux
+        {
+            get
+            {
+                return this.taux;
+            }
+
+            set
+            {
+                this.taux = value;
+            }
+        }
+    }
+}
